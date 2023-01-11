1a70069c8d2f8ec7f5bfe90d9342785d9136de52
User <User@WIN-E8F88FA2UKH>
2023-01-12 02:23:18 +0300

    d

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -1,9 +1,15 @@
+
 #include<iostream>
 using namespace std;
+//#define VAR_1
+#define tab "\t"
+
+
 
 void main()
 {
 	setlocale(LC_ALL, "");
+#if defined VAR_1
 	int b = 0;
 	const int n = 5;
 	int arr[n] = { 3,5,8 };
@@ -30,8 +36,45 @@ void main()
 
 	cout << endl;
 	
+	int min, max;
+	min = max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Мининальное значение в массиве: " << min << endl;
+	cout << "Максимальное значение в массиве:" << max << endl;
+#endif
+	
+	const int n = 10;
+	//Вывод исходного массива на экран
+	int arr[n] = { 0,1,2,3,4,5,6,7,8,9 };
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+	//Сдвиг массива
+	int number_of_shifts;
+	cout << "Сдвиг массива на: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+			for (int i = 1; i < n; i++)
+			{
+				arr[i - 1] = arr[i];
+			}
+		arr[n - 1] = buffer;
+	}
 
 
 
+	//вывод сдвинутого массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
 
 }
