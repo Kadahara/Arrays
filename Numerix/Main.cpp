7b1b49fcc97e57595c5e1ce8876a7ca8f46e8cb3
User <User@WIN-E8F88FA2UKH>
2023-01-12 02:25:03 +0300

    Домашняя работа тест гид хаб

--- Numerix/Main.cpp
+++ Numerix/Main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 //#define DECIMAL
 void main()
 {
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "RU");
 #if defined DECIMAL
 	int decimal;
 	cout << "Введите десятичное число: "; cin >> decimal;
